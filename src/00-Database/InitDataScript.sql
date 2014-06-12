093312e4e03ac1d85d09121ecea9627f8009f2bc
nick <zhengyanming2006@163.com>
2014-06-12 21:20:44 +0800

    Clear struct.

--- src/00-Database/InitDataScript.sql
+++
@@ -1,4 +0,0 @@
-SET IDENTITY_INSERT [Users] ON
-Insert Into [Users] ([SysNo],[UserID],[UserPwd]) Values('10001','TestUser','202cb962ac59075b964b07152d234b70')
-Insert Into [Users] ([SysNo],[UserID],[UserPwd]) Values('10002','TestUser1','202cb962ac59075b964b07152d234b70')
-SET IDENTITY_INSERT [Users] OFF
--- src/02-Web/Analysis28/Analysis28.WebUI/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Analysis28.WebUI.MvcApplication" Language="C#" %>
